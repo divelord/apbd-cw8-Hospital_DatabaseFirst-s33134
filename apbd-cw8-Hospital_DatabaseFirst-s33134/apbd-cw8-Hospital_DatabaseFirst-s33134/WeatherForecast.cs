5766e18cc4a555a445ffb84f169ff12e5c21c775
Michal Fukowski <michal01f@gmail.com>
2026-05-22 14:10:49 +0200

    initial project

---
+++ apbd-cw8-Hospital_DatabaseFirst-s33134/apbd-cw8-Hospital_DatabaseFirst-s33134/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace apbd_cw8_Hospital_DatabaseFirst_s33134;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
