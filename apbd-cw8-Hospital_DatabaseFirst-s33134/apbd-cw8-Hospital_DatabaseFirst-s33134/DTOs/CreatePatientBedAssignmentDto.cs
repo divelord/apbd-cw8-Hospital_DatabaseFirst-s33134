98be491d6152607c199d475003a5d450873d1c5e
Michal Fukowski <michal01f@gmail.com>
2026-05-22 14:35:20 +0200

    add DTOs

---
+++ apbd-cw8-Hospital_DatabaseFirst-s33134/apbd-cw8-Hospital_DatabaseFirst-s33134/DTOs/CreatePatientBedAssignmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_cw8_Hospital_DatabaseFirst_s33134.DTOs;
+
+public class CreatePatientBedAssignmentDto
+{
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    public string BedType { get; set; } = string.Empty;
+    public string Ward { get; set; } = string.Empty;
+}
---
+++ apbd-cw8-Hospital_DatabaseFirst-s33134/apbd-cw8-Hospital_DatabaseFirst-s33134/DTOs/GetPatientDetailsDto.cs
@@ -0,0 +1,64 @@
+﻿namespace apbd_cw8_Hospital_DatabaseFirst_s33134.DTOs;
+
+public class GetPatientDetailsDto
+{
+    public string Pesel { get; set; } = string.Empty;
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Sex { get; set; } = string.Empty;
+    public List<GetPatientAdmissionDetailsDto> Admissions { get; set; } = [];
+    public List<GetPatientBedAssignmentDetailsDto> BedAssignments { get; set; } = [];
+
+}
+
+public class GetPatientAdmissionDetailsDto
+{
+    public int Id { get; set; }
+    public DateTime AdmissionDate { get; set; }
+    public DateTime? DischargeDate { get; set; }
+    public GetPatientAdmissionWardDetailsDto Ward { get; set; } = null!;
+}
+
+public class GetPatientAdmissionWardDetailsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public String Description { get; set; } = string.Empty;
+}
+
+public class GetPatientBedAssignmentDetailsDto
+{
+    public int Id { get; set; }
+    public DateTime From { get; set; }
+    public DateTime? To { get; set; }
+    public GetPatientBedAssignmentBedDetailsDto Bed { get; set; } = null!;
+}
+
+public class GetPatientBedAssignmentBedDetailsDto
+{
+    public int Id { get; set; }
+    public GetPatientBedAssignmentBedTypeDetailsDto BedType { get; set; } = null!;
+    public GetPatientBedAssignmentBedRoomDetailsDto Room { get; set; } = null!;
+}
+
+public class GetPatientBedAssignmentBedTypeDetailsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
+public class GetPatientBedAssignmentBedRoomDetailsDto
+{
+    public string Id { get; set; } = string.Empty;
+    public bool HasTv { get; set; }
+    public GetPatientBedAssignmentBedRoomWardDetailsDto Ward { get; set; } = null!;
+}
+
+public class GetPatientBedAssignmentBedRoomWardDetailsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
